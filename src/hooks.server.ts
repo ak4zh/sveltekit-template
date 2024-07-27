f4f0a5116edfe3276b5a750bd52afe5c30a91a62
ak4zh <agwl.akash@gmail.com>
2024-07-27 14:07:53 +0530

    cleanup server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,16 +2,19 @@ import { lucia } from '$lib/server/lucia';
 import { type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { i18n, redirectI18n } from '$lib/i18n';
+import type { RequestEvent } from '@sveltejs/kit';
 
-export const authHandle: Handle = async ({ event, resolve }) => {
+const isPublicRoute = (event: RequestEvent) => event.route.id?.startsWith('/(public)');
+const isPrivateRoute = (event: RequestEvent) => event.route.id?.startsWith('/(private)');
+const isAdminRoute = (event: RequestEvent) => event.route.id?.startsWith('/(private)/(admin)');
+
+export const sessionHandle: Handle = async ({ event, resolve }) => {
 	const startTimer = Date.now();
 	event.locals.startTimer = startTimer;
-
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 	const { session, user } = sessionId
 		? await lucia.validateSession(sessionId)
 		: { session: null, user: null };
-
 	if (session && session.fresh) {
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
@@ -28,14 +31,21 @@ export const authHandle: Handle = async ({ event, resolve }) => {
 	}
 	event.locals.user = user;
 	event.locals.session = session;
-	if (event.route.id?.startsWith('/(private)')) {
-		if (!user) return redirectI18n(302, '/login', event);
+	return resolve(event);
+};
+
+export const authHandle: Handle = async ({ event, resolve }) => {
+	const user = event.locals.user;
+	if (isPublicRoute(event)) return resolve(event);
+	if (!user) return redirectI18n(302, '/login', event);
+
+	if (isPrivateRoute(event)) {
 		if (!user.emailVerified) return redirectI18n(302, '/verify/email', event);
 	}
-	if (event.route.id?.startsWith('/(private)/(admin)')) {
-		if (user?.role !== 'ADMIN') return redirectI18n(302, '/profile', event);
+	if (isAdminRoute(event)) {
+		if (user.role !== 'ADMIN') return redirectI18n(302, '/profile', event);
 	}
 	return resolve(event);
 };
 
-export const handle = sequence(i18n.handle(), authHandle);
+export const handle = sequence(i18n.handle(), sessionHandle, authHandle);
