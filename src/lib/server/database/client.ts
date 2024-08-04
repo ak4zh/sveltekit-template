7bcfc784e551acee602823689c1186d869066c2c
ak4zh <agwl.akash@gmail.com>
2024-08-04 11:52:00 +0530

    use node-postgres for jsonb support

--- src/lib/server/database/client.ts
+++ src/lib/server/database/client.ts
@@ -1,7 +1,8 @@
 import { DATABASE_URL } from '$env/static/private';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 
-const queryClient = postgres(DATABASE_URL);
-const db = drizzle(queryClient);
+const client = new Client({ connectionString: DATABASE_URL });
+await client.connect();
+const db = drizzle(client);
 export default db;
