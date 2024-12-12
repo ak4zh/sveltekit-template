4b37791afa99a1321e1e41706f4ef4e29812dada
ak4zh <agwl.akash@gmail.com>
2024-12-12 13:39:43 +0530

    add paraglibe types in locale

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { AvailableLanguageTag } from "$lib/paraglide/runtime.js"
+
 declare global {
 	namespace App {
 		interface Locals {
 			user: import('$lib/server/auth').SessionValidationResult['user'];
 			session: import('$lib/server/auth').SessionValidationResult['session'];
+			paraglide:  {
+				lang: AvailableLanguageTag
+				textDirection: "rtl" | "ltr"
+			};
 			startTimer: number;
 			error: string;
 			errorId: string;
